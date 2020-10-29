f5753238d0fbd39404a8d8617ac596b57b43e27c
raymond gitonga <raytosh95@gmail.com>
2020-10-29 14:53:18 +0300

    Add concurrency to data calls

--- email_service/service/user_service.go
+++ email_service/service/user_service.go
@@ -11,30 +11,59 @@ var (
 	UserService userService
 )
 
-func (u *userService)GetUser(email string) (*dormain.User, *utils.AppError) {
-	user, err := dormain.GetUser(email)
+func (u *userService) GetUser(email string) (*dormain.User, *utils.AppError) {
+	errChan := make(chan *utils.AppError, 1)
+	userChan := make(chan *dormain.User, 1)
 
-	if err!= nil {
+	go func() {
+		user, err := dormain.GetUser(email)
+		errChan <- err
+		userChan <- user
+	}()
+	user := <-userChan
+	err := <-errChan
+
+	if err != nil {
 		return nil, err
 	}
 
-	return user,nil
+	return user, nil
 }
 
-func (u *userService)GetUsers() (*[]dormain.User, *utils.AppError) {
-	users, err := dormain.GetUsers()
+func (u *userService) GetUsers() (*[]dormain.User, *utils.AppError) {
+	errChan := make(chan *utils.AppError, 1)
+	userChan := make(chan *[]dormain.User, 1)
 
-	if err!= nil {
+	go func() {
+		users, err := dormain.GetUsers()
+		userChan <- users
+		errChan <- err
+	}()
+
+	err := <-errChan
+	users := <-userChan
+
+	if err != nil {
 		return nil, err
 	}
-
 	return users, nil
 }
 
-func (u *userService)AddUser(user dormain.User) (*utils.SuccessResponse, *utils.AppError) {
-	resp, err := dormain.AddUser(user)
+func (u *userService) AddUser(user dormain.User) (*utils.SuccessResponse, *utils.AppError) {
 
-	if err!=nil {
+	errChan := make(chan *utils.AppError, 1)
+	respChan := make(chan *utils.SuccessResponse, 1)
+
+	go func() {
+		resp, err := dormain.AddUser(user)
+		respChan <- resp
+		errChan <- err
+	}()
+
+	err := <-errChan
+	resp := <-respChan
+
+	if err != nil {
 		return nil, err
 	}
 
