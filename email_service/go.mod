d217ca261c7c7b1ffa247c2d2ceb308700a349ac
raymond gitonga <raytosh95@gmail.com>
2020-10-30 17:58:18 +0300

    Add service layer tests

--- email_service/go.mod
+++ email_service/go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/assert/v2 v2.0.1
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/golang/protobuf v1.4.3 // indirect
@@ -12,7 +13,7 @@ require (
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/stretchr/testify v1.6.1
 	github.com/ugorji/go v1.1.13 // indirect
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
 	golang.org/x/sys v0.0.0-20201024232916-9f70ab9862d5 // indirect
--- email_service/service/user_service_test.go
+++ email_service/service/user_service_test.go
@@ -1,1 +1,114 @@
 package service
+
+import (
+	"github.com/raymondgitonga/nubi_service/email_service/dormain"
+	"github.com/raymondgitonga/nubi_service/email_service/utils"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type userDaoMock struct{}
+
+var (
+	getUserFunction  func(email string) (*dormain.User, *utils.AppError)
+	getUsersFunction func() (*[]dormain.User, *utils.AppError)
+	addUserFunction  func(user dormain.User) (*utils.SuccessResponse, *utils.AppError)
+	user             = dormain.User{Name: "Raymond", Email: "raytosh95@gmail.com"}
+	err              = utils.AppError{Message: "user not found", StatusCode: 500}
+)
+
+func init() {
+	dormain.UserDaoInterface = &userDaoMock{}
+}
+
+func (u userDaoMock) GetUser(email string) (*dormain.User, *utils.AppError) {
+	return getUserFunction(email)
+}
+func (u userDaoMock) GetUsers() (*[]dormain.User, *utils.AppError) {
+	return getUsersFunction()
+}
+
+func (u userDaoMock) AddUser(user dormain.User) (*utils.SuccessResponse, *utils.AppError) {
+	return addUserFunction(user)
+}
+
+func TestUserService_GetUser(t *testing.T) {
+	getUserFunction = func(email string) (*dormain.User, *utils.AppError) {
+		return &user, nil
+	}
+
+	user, err := UserServiceInterface.GetUser("raytosh95@gmail.com")
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.Equal(t, "raytosh95@gmail.com", user.Email)
+}
+
+func TestUserService_GetUserError(t *testing.T) {
+	getUserFunction = func(email string) (*dormain.User, *utils.AppError) {
+		return nil, &err
+	}
+
+	user, err := UserServiceInterface.GetUser("raytosh95@gmail.com")
+
+	assert.Nil(t, user)
+	assert.NotNil(t, err)
+	assert.Equal(t, 500, err.StatusCode)
+}
+
+func TestUserService_GetUsers(t *testing.T) {
+	getUsersFunction = func() (*[]dormain.User, *utils.AppError) {
+		return &[]dormain.User{
+			user, user,
+		}, nil
+	}
+
+	users, err := UserServiceInterface.GetUsers()
+
+	assert.NotNil(t, users)
+	assert.Nil(t, err)
+	assert.Equal(t, "Raymond", (*users)[0].Name)
+}
+
+func TestUserService_GetUsersError(t *testing.T) {
+	getUsersFunction = func() (*[]dormain.User, *utils.AppError) {
+		return nil, &err
+	}
+
+	users, err := UserServiceInterface.GetUsers()
+
+	assert.NotNil(t, err)
+	assert.Nil(t, users)
+	assert.Equal(t, 500, err.StatusCode)
+}
+
+func TestUserService_AddUser(t *testing.T) {
+	addUserFunction = func(user dormain.User) (*utils.SuccessResponse, *utils.AppError) {
+		return &utils.SuccessResponse{
+			Message:    "successfully added user",
+			StatusCode: 200,
+		}, nil
+	}
+
+	user = user
+	resp, err := UserServiceInterface.AddUser(user)
+
+	assert.NotNil(t, resp)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
+}
+
+func TestUserService_AddUserError(t *testing.T) {
+	addUserFunction = func(user dormain.User) (*utils.SuccessResponse, *utils.AppError) {
+		return nil, &err
+	}
+	user := dormain.User{
+		Name:  " ",
+		Email: " ",
+	}
+	resp, err := UserServiceInterface.AddUser(user)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, resp)
+	assert.Equal(t, 500, err.StatusCode)
+}
